395a32ff204f9f5c3381e5eecf9571747858b007
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-08-11 15:48:47 -0500

    added basic fragment shader still need to undertand it

---
+++ sketches/shader/fragmnent.glsl
@@ -0,0 +1,13 @@
+uniform float time;
+uniform float progress;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform vec4 resolution;
+uniform vec2 vUv;
+uniform vec4 vPosition;
+float PI = 3.14159265359;
+
+void main(){
+  gl_FragColor=vec4(vUv,0.,1.)
+}
+
