b512f8300ebc5c125a02291fd9f1f3409a72dd99
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2020-08-11 19:16:39 -0500

    added 3d perlin noise funciton

--- sketches/shader/vertex.glsl
+++ sketches/shader/vertex.glsl
@@ -1,5 +1,8 @@
 varying vec2 vUv;
 void main() {
+  // creating a copy 'varying' of the texture coordinates
+  //-- Varying ==> are variables that are passed from the vertex shader to the fragment shader. For each fragment, the value of each varying will be smoothly interpolated from the values of adjacent vertices.
   vUv = uv;
+  // Note that you can therefore calculate the position of a vertex in the vertex shader by:
  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
